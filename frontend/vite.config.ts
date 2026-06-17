6a9695b733bacf029489eb2ee5779b8d73bb4591
Gustavo H. Martins <gusmartins06@gmail.com>
2026-06-17 00:31:08 -0300

    tests: fix pipeline

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
 
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html', 'lcov'],
+      reporter: ['text', 'json', 'html', 'lcovonly'],
       reportsDirectory: './coverage',
 
       include: ['src/**/*.{ts,tsx}'],
